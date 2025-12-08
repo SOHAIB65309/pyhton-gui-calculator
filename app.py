7bba3f59709b49371bc7a52471e615ffca8de2bc
SOHAIB65309 <sohaibilyas530@gmail.com>
2025-12-08 09:56:17 +0500

    initial

---
+++ app.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+def calculate_sum():
+  try:
+    num1 = float(entry1.get())
+    num2 = float(entry2.get())
+    result.set(num1 + num2)
+  except ValueError:
+    result.set("Invalid input")
+
+root = tk.Tk() 
+root.title("Simple Calculator")
+
+entry1 = tk.Entry(root) 
+entry1.pack()
+
+entry2 = tk.Entry(root) 
+entry2.pack()
+
+result = tk.StringVar()
+label = tk.Label(root, textvariable=result) 
+label.pack()
+
+button = tk.Button(root, text="Add", command=calculate_sum) 
+button.pack()
+
+root.mainloop()
